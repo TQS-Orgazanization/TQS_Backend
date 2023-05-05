731ce63d28ef52a68788ff436787c7c9aa13b87c
Rui Delfim De Oliveira <rui.junior@ua.pt>
2023-05-05 11:01:05 +0100

    CreateSpringProject

---
+++ PickUpBackEnd/src/main/java/com/example/pickupbackend/PickUpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.pickupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickUpBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PickUpBackEndApplication.class, args);
+    }
+
+}
---
+++ PickUpBackEnd/src/test/java/com/example/pickupbackend/PickUpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pickupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickUpBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
